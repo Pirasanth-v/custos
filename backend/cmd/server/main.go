2b830cb0ead5c1b8cabf35bef652f98979b3efe7
pirasanth-v <pirasanth.v3@gmail.com>
2026-03-07 17:58:44 +0530

    chore(backend): setup database connection pool with pgx

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,18 +3,32 @@ package main
 import (
 	"log"
 	"log/slog"
-	
-	config "github.com/Pirasanth-v/custos/internal/config"
+	"context"
+
+	config "github.com/pirasanth-v/custos/internal/config"
+	db "github.com/pirasanth-v/custos/internal/database"
 )
 
 func main() {
+	// Load the Config
 	cfg, err := config.Load()
-
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
-
 	slog.Info("Cfg is loaded and ready to use")
 
-	_ = cfg
+	// Connect to DB
+	db, err := db.Connect(cfg.DB)
+	if err != nil {
+		log.Fatalf("Failed to connect database: %v", err)
+	}
+	slog.Info("db connection created successfully")
+	defer db.Close()
+
+	// DB healthcheck 
+	if err := db.Ping(context.Background()); err != nil {
+		db.Close()
+		log.Fatalf("Unable to ping database: %v", err)
+	}
+
 }
