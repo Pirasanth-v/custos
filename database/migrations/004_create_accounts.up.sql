4268b2a9cd781641b521ca54008fecf2971d3c93
pirasanth-v <pirasanth.v3@gmail.com>
2026-03-30 19:30:37 +0530

    fix: add deleted_by column to accounts table

--- database/migrations/004_create_accounts.up.sql
+++ database/migrations/004_create_accounts.up.sql
@@ -1,4 +1,4 @@
-CREATE TYPE account_type AS ENUM ('bank', 'cash', 'credit_card', 'loan', 'wallet','other');
+CREATE TYPE account_type AS ENUM ('bank', 'cash', 'credit', 'wallet','other');
 
 CREATE TABLE accounts (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -9,6 +9,7 @@ CREATE TABLE accounts (
     net_balance NUMERIC(19,4) NOT NULL DEFAULT 0,
     description TEXT,
     created_by UUID NOT NULL REFERENCES users(id),
+    deleted_by UUID NULL REFERENCES user(id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     deleted_at TIMESTAMPTZ 
