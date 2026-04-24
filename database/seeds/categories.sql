b199dc988d0fbb89aead172fbed761e3a004fa34
pirasanth-v <pirasanth.v3@gmail.com>
2026-04-24 19:47:32 +0530

    fix: remove seeds, resolve unique name

--- database/seeds/categories.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO categories (id, name, created_by, created_at, updated_at, deleted_at)
-VALUES
-    ('a3f45e70-8ec2-4c14-920b-1d428faf0011', 'Groceries',  '5d2c1943-e37c-4535-9524-81372f7fac2b', NOW(), NOW(), NULL)
